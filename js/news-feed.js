6d5112bfdeb20a53d3f78b2bbf5c2b89b8924846
Kannan Sanjeevan <Kannan_Sanjeevan@epam.com>
2019-03-27 16:44:14 +0530

    - Improved Code, - Added Hyperlink to news title - Modified hyperlink styling

--- js/news-feed.js
+++ js/news-feed.js
@@ -13,16 +13,14 @@ class Newsfeed{
         return fetch(this.sourcesAPI).
                 then( response => response.json()).
                 then( data => this.generateNewsChannelSelectElement(data)).
-                catch(function(error){
-                    console.log(error);
-            });
+                catch( error => console.log(error));
     }
 
     generateNewsChannelSelectElement( data ){
-        let selectElement = document.getElementById('channelFilter');
+        const selectElement = document.getElementById('channelFilter');
         let selectElementOptions='<option value="select-channel">Select Channel</option>';
-        data.sources.map( source => {
-            selectElementOptions += `<option value='${source.id}'>${source.name}</option>`;
+        data.sources.map(({ id, name }) => {
+            selectElementOptions += `<option value='${id}'>${name}</option>`;
         });
         selectElement.innerHTML = selectElementOptions;
     }
@@ -31,28 +29,25 @@ class Newsfeed{
     // To Handle 'change' event
     handleEvents(){
         const channelFilter = document.getElementById('channelFilter');
-
-        channelFilter.addEventListener('change', (e) => {
-             if( e.target.tagName === 'SELECT'){
-                 this.getNewsByChannel(e.target.value);
+        channelFilter.addEventListener('change', ({ target }) => {
+             if( target.tagName === 'SELECT'){
+                 this.getNewsByChannel(target.value);
              }
         });
     }
     
     getNewsByChannel( channelID ){
         const channelAPI = `https://newsapi.org/v1/articles?source=${channelID}&apiKey=${this.key}`;
-
         return fetch(channelAPI).
                 then(response => response.json()).
                 then(data => this.renderNews(data)).
-                catch(function(error){
-                    console.log(error);
-                });
+                catch( error => console.log(error));
     }
 
     renderNews( data ){
-        let newsContainer = document.getElementById('app-main__news');
+        const newsContainer = document.getElementById('app-main__news');
         let newsContent=`<h1 tabindex="0" class="app-main__news__title">${data.source}</h1>`;
+
         data.articles.map(news => { 
             newsContent += this.generateNewsHTML(news.author, news.title, news.description, news.urlToImage, news.url, news.publishedAt);
         });
@@ -69,7 +64,7 @@ class Newsfeed{
                         <img src="${image}" alt="${title}" />
                     </div>
                     <div class="news__body">
-                        <h3 class="news__title">${title}</h3>
+                        <h3 class="news__title"><a href="${url}" target="_blank">${title}</a></h3>
                         
                         <div class="news__meta">
                             <p>Author: <b><em class="news__meta__author">${author}</em></b> // Published on <b>${ this.formateDate(publishedOn) }</b></p>
@@ -88,11 +83,11 @@ class Newsfeed{
 
     // helper 
     formateDate(dateString){
-        let date = new Date(dateString);
-        let day = date.getDate();
-        let month = date.getMonth() + 1;
-        let year = date.getFullYear();
-        let modifiedDate = `${day}/${month}/${year}`;
+        const date = new Date(dateString);
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        const modifiedDate = `${day}/${month}/${year}`;
 
         return modifiedDate;
     }
